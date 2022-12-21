0b1dcdb65a67af088e89ea8b8641c05fdea83bbe
eddiefuller <30291001+eddiefuller@users.noreply.github.com>
2022-12-21 18:05:27 -0500

    Update mol_graph_conv_featurizer.py

--- deepchem/feat/molecule_featurizers/mol_graph_conv_featurizer.py
+++ deepchem/feat/molecule_featurizers/mol_graph_conv_featurizer.py
@@ -217,8 +217,10 @@ class MolGraphConvFeaturizer(MolecularFeaturizer):
     h_bond_infos = construct_hydrogen_bonding_info(datapoint)
     
     if self.use_geom:
+      print("Using atom geometry features.")
       mol_geom = get_geom(datapoint,hydro=True)
     else:
+      print("Without geometry features.")
       mol_geom = {}
       
      
